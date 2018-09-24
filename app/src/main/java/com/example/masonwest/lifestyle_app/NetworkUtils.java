670e5369d91612d623ff903ef6655e96c55f56f3
Mason West <mew0830@gmail.com>
2018-09-23 23:32:48 -0600

    added supporting files to allow weather data json to be loaded. Issue with loader and URI call.

---
+++ app/src/main/java/com/example/masonwest/lifestyle_app/NetworkUtils.java
@@ -0,0 +1,50 @@
+package com.example.masonwest.lifestyle_app;
+
+import android.net.Uri;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class NetworkUtils {
+    private static String BASE_URL = "http://api.openweathermap.org/data/2.5/weather?q=";
+    private static String APPIDQUERY = "&appid=";
+    private static final String app_id="7b8fad25eeac14a746e113dd2c21608e"; //7b8fad25eeac14a746e113dd2c21608e
+
+    public static URL buildURLFromString(String location){
+        URL myURL = null;
+        try{
+            myURL = new URL(BASE_URL + location + APPIDQUERY + app_id);
+        }catch(MalformedURLException e){
+            e.printStackTrace();
+        }
+        return myURL;
+    }
+
+    public static String getDataFromURL(URL url) throws IOException{
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream inputStream = urlConnection.getInputStream();
+
+            //The scanner trick: search for the next "beginning" of the input stream
+            //No need to user BufferedReader
+            Scanner scanner = new Scanner(inputStream);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if(hasInput){
+                return scanner.next();
+            }
+            else{
+                return null;
+            }
+
+        }finally {
+            urlConnection.disconnect();
+        }
+    }
+}
+
