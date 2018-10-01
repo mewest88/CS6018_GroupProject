469a9d32ab73d4da8e6c03f726e4d29309c40bd1
christopherroper <croper@alumni.nd.edu>
2018-10-01 12:14:15 -0600

    view model shiz

--- app/src/main/java/com/example/masonwest/lifestyle_app/UserRepository.java
+++ app/src/main/java/com/example/masonwest/lifestyle_app/UserRepository.java
@@ -3,14 +3,19 @@ package com.example.masonwest.lifestyle_app;
 import android.app.Application;
 import android.arch.lifecycle.MutableLiveData;
 
+import java.util.List;
+
 public class UserRepository {
     private final MutableLiveData<User> userData = new MutableLiveData<>() ;
-
+    private MutableLiveData<List<User>> mAllUsers;
 
     UserRepository(Application application) {
-        loadData() ;
+        UserRoomDatabase db = UserRoomDatabase.getDatabase(application);
+        mUserDao = db.userDao();
+        mAllUsers = mUserDao.getAllWords();
     }
 
+    MutableLiveData<List<User>> getAllUsers() { return mAllUsers; }
     private void loadData() {
 
     }
