c4f3a12bbb0bb54abf92ad22322b2abd2e85f492
Madeline Luke <madeline.luke@utah.edu>
2018-10-03 11:26:01 -0600

    added database and filled in all the skeletons

--- app/src/main/java/com/example/masonwest/lifestyle_app/UserRepository.java
+++ app/src/main/java/com/example/masonwest/lifestyle_app/UserRepository.java
@@ -1,22 +1,42 @@
 package com.example.masonwest.lifestyle_app;
 
 import android.app.Application;
+import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
+import android.os.AsyncTask;
 
 import java.util.List;
 
 public class UserRepository {
     private final MutableLiveData<User> userData = new MutableLiveData<>() ;
     private MutableLiveData<List<User>> mAllUsers;
+    private UserDao mUserDao ;
 
     UserRepository(Application application) {
-        UserRoomDatabase db = UserRoomDatabase.getDatabase(application);
+        UserDatabase db = UserDatabase.getDatabase(application);
         mUserDao = db.userDao();
-        mAllUsers = mUserDao.getAllWords();
+        mAllUsers = mUserDao.getAllUsers();
+    }
+
+    public void insert(User user) {
+        new insertAsyncTask(mUserDao).execute(user) ;
     }
 
     MutableLiveData<List<User>> getAllUsers() { return mAllUsers; }
-    private void loadData() {
 
+    // AsyncTask class
+    private static class insertAsyncTask extends AsyncTask<User, Void, Void> {
+
+        private UserDao mAsyncTaskDao;
+
+        insertAsyncTask(UserDao dao) {
+            mAsyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(final User... params) {
+            mAsyncTaskDao.insert(params[0]);
+            return null;
+        }
     }
 }
