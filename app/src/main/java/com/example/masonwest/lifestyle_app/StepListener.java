4811054d89c9d43bc431d411f3ac2acc8bbc825b
Mason West <mew0830@gmail.com>
2018-10-23 10:34:14 -0600

    added post used to create step counter

--- app/src/main/java/com/example/masonwest/lifestyle_app/StepListener.java
+++ app/src/main/java/com/example/masonwest/lifestyle_app/StepListener.java
@@ -1,5 +1,8 @@
 package com.example.masonwest.lifestyle_app;
 
+// Used this amazing blog to build step detector
+// http://www.gadgetsaint.com/android/create-pedometer-step-counter-android/#.W832EVJMHOQ
+
 // Will listen to step alerts
 public interface StepListener {
 
